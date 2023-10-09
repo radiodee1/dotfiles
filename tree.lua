554d9768c90e91e9ad3c544b9082d4e68281275a
David Liebman <radiodee1@gmail.com>
2023-10-09 16:18:24 -0400

    tree doesnt focus right

--- tree.lua
+++ tree.lua
@@ -286,25 +286,25 @@ local function open_nvim_tree(data)
   -- buffer is a [No Name]
   local no_name = data.file == "" and vim.bo[data.buf].buftype == ""
 
+  -- only files please
   if not real_file and not no_name then
     return
   end
 
-  -- open the tree, find the file but don't focus it
-  require("nvim-tree.api").tree.toggle({ focus = true, find_file = true, })
+  -- open the tree but don't focus it
+  require("nvim-tree.api").tree.toggle({ focus = false })
+
 end
 
 
 vim.api.nvim_create_autocmd({ "VimEnter" }, { callback = open_nvim_tree }) 
 
----[[
 require("nvim-tree.api").tree.open({
 	path = nil,
 	current_window = false,
 	find_file = true,
 	update_root = false,
 })
---]]
 
 -- close window when last tab is closed
 local function tab_win_closed(winnr)
