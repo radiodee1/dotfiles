357b399b65eae9a4a61f9ec0c1f4b002c08835da
David Liebman <radiodee1@gmail.com>
2024-04-17 17:15:36 -0400

    use name instead of ip address

--- smb-mount-edit.sh
+++ smb-mount-edit.sh
@@ -1,6 +1,8 @@
 if [ -d /mnt/smb  ]; then
-    mount -t cifs //192.168.0.123/edit /mnt/smb 
+    mount -t cifs //truenas.local/edit /mnt/smb 
 else
     echo "could not find /mnt/smb"
 
 fi
+
+## 192.168.0.123
