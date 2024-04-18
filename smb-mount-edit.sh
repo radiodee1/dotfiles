46ce87404fffd23d85707e5a2fd1816d09abd55c
David Liebman <radiodee1@gmail.com>
2024-04-18 10:53:56 -0400

    enter password at cmd line

--- smb-mount-edit.sh
+++ smb-mount-edit.sh
@@ -12,7 +12,7 @@ if [ -d /mnt/smb/edit  ]; then
     mount -t cifs //192.168.0.123/edit /mnt/smb/edit -o username=dave,password=$PASS,uid=1000
 else
     echo "could not find /mnt/smb/edit"
-
+    exit
 fi
 
 ## 192.168.0.123
