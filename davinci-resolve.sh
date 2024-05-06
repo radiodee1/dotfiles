bbcb9e3efcfd180e4905ea20e495fa27f868612c
David Liebman <radiodee1@gmail.com>
2024-05-06 15:03:35 -0400

    davinci resolve starts - no gpu

--- davinci-resolve.sh
+++ davinci-resolve.sh
@@ -1,3 +1,5 @@
-LD_PRELOAD=/usr/lib/x86_64-linux-gnu/libglib-2.0.so.0 /opt/resolve/bin/resolve
+#export LD_LIBRARY_PATH="/usr/lib/x86_64-linux-gnu/"
+
+LD_PRELOAD="/usr/lib/x86_64-linux-gnu/libglib-2.0.so /usr/lib/x86_64-linux-gnu/libgdk-3.so.0 /usr/lib/x86_64-linux-gnu/libpango-1.0.so.0 /usr/lib/x86_64-linux-gnu/libgio-2.0.so.0 /usr/lib/x86_64-linux-gnu/libgmodule-2.0.so.0" /opt/resolve/bin/resolve
 
 
