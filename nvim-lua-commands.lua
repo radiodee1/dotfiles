b851ba5609fbfb73cb9c49f6d7b1fcf49fbe1c41
David Liebman <radiodee1@gmail.com>
2024-06-12 13:57:15 -0400

    naming convention

--- nvim-lua-commands.lua
+++ nvim-lua-commands.lua
@@ -2,7 +2,3 @@
 
 
 vim.api.nvim_create_user_command('Markdown', ':! pandoc -o /tmp/md-temp.html -s -c $HOME/.config/nvim/md.css % && xdg-open /tmp/md-temp.html ', { nargs = 0 })
--- :command! -nargs=1 Upper echo toupper(<q-args>)
--- vim.api.nvim_create_user_command('Markdown', 'ls <q-args>', { nargs = 1 })
-
--- vim.cmd('Upper hello world') -- prints "HELLO WORLD"
