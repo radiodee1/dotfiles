79ddaaa71ba8ac2542832b9d6a3c2dc9cf1ef7d4
David Liebman <radiodee1@gmail.com>
2024-06-13 14:34:01 -0400

    description

--- nvim-lua-commands.lua
+++ nvim-lua-commands.lua
@@ -1,5 +1,4 @@
-
-
+-- some arbitrary commands for what might some day be an obsidian note-taking system (without obsidian)
 
 vim.api.nvim_create_user_command('Markdown', ':! pandoc -o /tmp/md-temp.html -s -c $HOME/.config/nvim/md.css "%" && xdg-open /tmp/md-temp.html ', { nargs = 0 })
 
