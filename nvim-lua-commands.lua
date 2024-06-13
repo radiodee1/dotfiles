ed9de0debb133bcd378d6b202fc539072c560951
David Liebman <radiodee1@gmail.com>
2024-06-13 13:59:55 -0400

    make html file with pandoc, convert link to work with html files using sed, and view in browser with xdg-open

--- nvim-lua-commands.lua
+++ nvim-lua-commands.lua
@@ -3,4 +3,4 @@
 
 vim.api.nvim_create_user_command('Markdown', ':! pandoc -o /tmp/md-temp.html -s -c $HOME/.config/nvim/md.css "%" && xdg-open /tmp/md-temp.html ', { nargs = 0 })
 
-vim.api.nvim_create_user_command('MarkdownInPlace', ':! pandoc -o "%.html" -s -c $HOME/.config/nvim/md.css "%" && xdg-open "%.html" ', { nargs = 0 })
+vim.api.nvim_create_user_command('MarkdownInPlace', ':! pandoc -o "%.html" -s -c $HOME/.config/nvim/md.css "%" && sed -i  \'s/.md"/.md.html"/g\' "%.html" && xdg-open "%.html" ', { nargs = 0 })
