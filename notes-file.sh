4e30be3ca6622f2bc3c22b1ac68f01a50dcf3be6
David Liebman <radiodee1@gmail.com>
2024-06-14 15:49:50 -0400

    dont allow editing of html files

--- notes-file.sh
+++ notes-file.sh
@@ -1,3 +1,10 @@
+html_from_md () {
+    NAME=$1
+    pandoc -o "$NAME.html" -s -c $HOME/.config/nvim/md.css "$NAME" 
+    sed -i  's/.md"/.md.html"/g' "$NAME.html" 
+}
+
+
 if [ $# -eq 0 ]; then
     echo "must add text for title!!"
     exit
@@ -5,7 +12,15 @@ fi
 
 if [ $# -eq 1 ]; then
     if [ -f $1  ]; then
+
+        FULLNAME=$1
+        if [ "${FULLNAME: -5}" == ".html" ]; then
+            echo 'do not edit html!'
+            exit 
+        fi
+        
         nvim $1
+        html_from_md $1
         exit
     fi
 fi
@@ -28,4 +43,7 @@ echo $NAME
 if [ -d "$HOME/Documents/vault/" ]; then
     touch $NAME
     nvim $NAME
+    html_from_md $NAME
 fi
+
+
