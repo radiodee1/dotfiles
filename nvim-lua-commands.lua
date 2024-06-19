994c7d0d6fb8e44e25ff0b5a42960fe5bc1ecbdf
David Liebman <radiodee1@gmail.com>
2024-06-19 07:22:32 -0400

    MarkdownFolder implemented ??

--- nvim-lua-commands.lua
+++ nvim-lua-commands.lua
@@ -4,4 +4,6 @@ vim.api.nvim_create_user_command('Markdown', ':! pandoc -o /tmp/md-temp.html -s 
 
 vim.api.nvim_create_user_command('MarkdownInPlace', ':! pandoc -o "%.html" -s -c $HOME/.config/nvim/md.css "%" && sed -i  \'s/.md"/.md.html"/g\' "%.html" && xdg-open "%.html" ', { nargs = 0 })
 
-local command =  ':! pandoc -o "%.html" -s -c $HOME/.config/nvim/md.css "%" && sed -i  \'s/.md"/.md.html"/g\' "%.html" && xdg-open "%.html" ' 
+local command =  ':! VAULT="$HOME/$(cat $HOME/.config/nvim/vault.txt)*.md" && for i in $(ls -1 $VAULT); do  (pandoc -o "$i.html" -s -c $HOME/.config/nvim/md.css "$i" && sed -i  \'s/.md"/.md.html"/g\' "$i.html"); done ' 
+
+vim.api.nvim_create_user_command('MarkdownFolder', command, { nargs = 0 })
