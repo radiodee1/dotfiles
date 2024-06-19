5e328432e63f55777c021bc583ce038d42edd7f7
David Liebman <radiodee1@gmail.com>
2024-06-19 06:52:51 -0400

    add vault text file

--- nvim-lua-commands.lua
+++ nvim-lua-commands.lua
@@ -3,3 +3,5 @@
 vim.api.nvim_create_user_command('Markdown', ':! pandoc -o /tmp/md-temp.html -s -c $HOME/.config/nvim/md.css "%" && xdg-open /tmp/md-temp.html ', { nargs = 0 })
 
 vim.api.nvim_create_user_command('MarkdownInPlace', ':! pandoc -o "%.html" -s -c $HOME/.config/nvim/md.css "%" && sed -i  \'s/.md"/.md.html"/g\' "%.html" && xdg-open "%.html" ', { nargs = 0 })
+
+local command =  ':! pandoc -o "%.html" -s -c $HOME/.config/nvim/md.css "%" && sed -i  \'s/.md"/.md.html"/g\' "%.html" && xdg-open "%.html" ' 
