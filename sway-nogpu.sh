69e0406e4bd6204ef9a425166ca87e28596b2394
David Liebman <radiodee1@gmail.com>
2025-12-31 07:48:26 -0500

    sway-nogpu.desktop install

--- sway-nogpu.sh
+++ sway-nogpu.sh
@@ -1,5 +1,5 @@
 
 if [ ! -f /usr/share/wayland-sessions/sway-nogpu.desktop ]; then
-
+    echo "Installing sway-nogpu.desktop wayland-sessions file."
     sudo cp sway-nogpu.desktop /usr/share/wayland-sessions/.
 fi
