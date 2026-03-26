66dc9cb3460ff761da0478ec304db757b69534e9
David Liebman <radiodee1@gmail.com>
2026-03-26 10:46:22 -0400

    ping nas

--- waybar-scripts-ping_nas.sh
+++ waybar-scripts-ping_nas.sh
@@ -13,7 +13,7 @@ HOST=$ip
 
 # Ping the host once with a 1-second timeout and suppress output
 if ping -c 4  $HOST &>/dev/null; then
-    echo "$HOST is reachable (up)"
+    echo "up"
 else
-    echo "$HOST is unreachable (down)"
+    echo "down"
 fi
