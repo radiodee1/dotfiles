c07c168e94e65b7509627ac85cce7c48798ad3fe
David Liebman <radiodee1@gmail.com>
2026-03-26 08:49:03 -0400

    scripting

---
+++ waybar-scripts-ping_nas.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+FILE="$HOME/.config/waybar/scripts/nas_ip.txt"
+
+if ! [ -f $FILE ]; then
+    echo "cannot find $FILE"
+    exit 1
+fi 
+
+
+read -r ip < "$FILE"
+HOST=$ip 
+
+# Ping the host once with a 1-second timeout and suppress output
+if ping -c 4  $HOST &>/dev/null; then
+    echo "$HOST is reachable (up)"
+else
+    echo "$HOST is unreachable (down)"
+fi
