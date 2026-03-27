0456b7075574b3bb96682caadf89e41a7a789c40
David Liebman <radiodee1@gmail.com>
2026-03-27 08:26:42 -0400

    ping nas with bash function

--- waybar-scripts-ping_nas.sh
+++ waybar-scripts-ping_nas.sh
@@ -11,37 +11,42 @@ fi
 read -r ip < "$FILE"
 HOST=$ip 
 
+snmp_info () {
+    OID_STRING=$1 
+    DISK1=$(snmpwalk -v 2c -c public $HOST $OID_STRING)
+    str=$DISK1
+    temp="${str#*\"}"  # Remove everything before the first "
+    result1="${temp%\"*}" # Remove everything after the last "
+    #echo $result1
+    length=${#result1}
+    if [ $length -gt 10 ]; then
+        result1='check'
+    fi
+    echo $result1
+    return 0
+}
+
+
 ping -c 4  $HOST &>/dev/null
 
 if [ $? -eq 0 ]; then
 
     
-    DISK1=$(snmpwalk -v 2c -c public $HOST .1.3.6.1.4.1.44738.5.1.1.4)
-    str=$DISK1
-    temp="${str#*\"}"  # Remove everything before the first "
-    result1="${temp%\"*}" # Remove everything after the last "
+    result=$(snmp_info .1.3.6.1.4.1.44738.5.1.1.4) 
     #echo $result1
-
-    if [ "$result1" = "degraded" ]; then
-        echo $result1
+    if [ "$result" = "degraded" ]; then
+        echo $result
         exit
     fi 
 
-    DISK1=$(snmpwalk -v 2c -c public $HOST .1.3.6.1.4.1.44738.4.1.1.5.1)
-
-    str=$DISK1
-    temp="${str#*\"}"  # Remove everything before the first "
-    result1="${temp%\"*}" # Remove everything after the last "
+    result1=$(snmp_info .1.3.6.1.4.1.44738.4.1.1.5.1)    
     #echo $result1
 
     length=${#result1}
 
     if [ $length -lt 10 ]; then
 
-        DISK2=$(snmpwalk -v 2c -c public $HOST .1.3.6.1.4.1.44738.4.1.1.5.2)
-        str=$DISK2
-        temp="${str#*\"}"  
-        result2="${temp%\"*}"
+        result2=$(snmp_info .1.3.6.1.4.1.44738.4.1.1.5.2) 
         length=${#result2}
         #echo $result2
         if [ $length -lt 10 ]; then
