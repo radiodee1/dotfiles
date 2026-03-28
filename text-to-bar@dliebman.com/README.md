1dbb5e441ef435d0b7a1c5510897b26a2d640664
David Liebman <radiodee1@gmail.com>
2026-03-28 08:58:16 -0400

    text-to-bar for gnome 47

--- text-to-bar@dliebman.com/README.md
+++ text-to-bar@dliebman.com/README.md
@@ -1,3 +1,8 @@
 # Debugging Extension
 
 journalctl -f -o cat /usr/bin/gnome-shell
+
+## Place Extension
+
+cp -r text-to-bar@dliebman.com ~/.local/share/gnome-shell/extensions/.
+
--- text-to-bar@dliebman.com/extension.js
+++ text-to-bar@dliebman.com/extension.js
@@ -1,47 +1,60 @@
-
 import { Extension } from 'resource:///org/gnome/shell/extensions/extension.js';
-import * as Main from 'resource:///org/gnome/shell/ui/main.js';
 import * as PanelMenu from 'resource:///org/gnome/shell/ui/panelMenu.js';
-import * as St from 'resource:///org/gnome/shell/widgets/st.js';
-import GLib from 'gi://GLib';
+import * as Main from 'resource:///org/gnome/shell/ui/main.js';
+import * as Clutter from 'gi://Clutter';
+import  GLib from 'gi://GLib';
+import  Gio from 'gi://Gio';
+import  St from 'gi://St';
 
-export default class CommandToBarExtension extends Extension {
+export default class CommandToPanelExtension extends Extension {
     enable() {
-        this._button = new PanelMenu.Button(0.0, this.metadata.name, false);
+        this._indicator = new PanelMenu.Button(0.0, this.metadata.name, false);
         this._label = new St.Label({
             text: 'Loading...',
-            y_align: St.Align.MIDDLE
+            y_align: 2// Clutter.ActorAlign.CENTER
         });
-        this._button.add_child(this._label);
-        Main.panel.addToStatusArea(this.metadata.uuid, this._button);
+        this._indicator.add_child(this._label);
+        Main.panel.addToStatusArea(this.metadata.uuid, this._indicator);
 
-        // Update every 30 seconds
-        this._timeout = GLib.timeout_add_seconds(GLib.PRIORITY_DEFAULT, 30, () => {
+        // Command to run (e.g., get date)
+        this._command = ['date', '+%H:%M:%S'];
+        
+        // Start the timer
+        this._timeoutId = GLib.timeout_add_seconds(GLib.PRIORITY_DEFAULT, 30, () => {
             this._updateLabel();
-            return GLib.SOURCE_CONTINUE;
+            return true; // Keep the timer alive
         });
-        this._updateLabel(); // Initial call
+        
+        // Initial call
+        this._updateLabel();
     }
 
     _updateLabel() {
-        // --- CHANGE YOUR COMMAND HERE ---
-        let command = 'date +%H:%M';  //'YOUR_COMMAND_HERE';
-        
         try {
-            let [res, out, err, status] = GLib.spawn_command_line_sync(command);
-            if (res) {
-                this._label.set_text(new TextDecoder().decode(out).trim());
-            } else {
-                this._label.set_text('Error');
-            }
+            // Run command asynchronously
+            let proc = new Gio.Subprocess({
+                argv: this._command,
+                flags: Gio.SubprocessFlags.STDOUT_PIPE
+            });
+            proc.init(null);
+            proc.communicate_utf8_async(null, null, (proc, res) => {
+                let [, stdout] = proc.communicate_utf8_finish(res);
+                this._label.set_text(stdout.trim());
+            });
         } catch (e) {
             this._label.set_text('Error');
+            console.error(e);
         }
     }
 
     disable() {
-        GLib.source_remove(this._timeout);
-        this._button.destroy();
-        this._button = null;
+        if (this._timeoutId) {
+            GLib.source_remove(this._timeoutId);
+            this._timeoutId = 0;
+        }
+        this._indicator.destroy();
+        this._indicator = null;
+        this._label = null;
     }
 }
+
