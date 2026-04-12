299e1d3c7a9a637f9e0bb36062fcb277597ae672
David Liebman <radiodee1@gmail.com>
2026-04-12 08:37:12 -0400

    blink.lua

--- nvim-lazy/lua/plugins/blink.lua
+++ nvim-lazy/lua/plugins/blink.lua
@@ -1,6 +1,33 @@
 return {
   "saghen/blink.cmp",
   opts = {
+    completion = {
+      menu = {
+        auto_show = true,
+      },
+      ghost_text = {
+        enabled = true,
+      },
+      -- list = {
+      --  selection = { preselect = false, auto_insert = false },
+      -- },
+    },
+    cmdline = {
+      enabled = true,
+      -- { "buffer" },
+      sources = function()
+        local type = vim.fn.getcmdtype()
+        -- Enable buffer-based completion for searching with / or ?
+        if type == "/" or type == "?" then
+          return { "buffer" }
+        end
+        -- Enable command-line completion for :
+        if type == ":" then
+          return { "cmdline" }
+        end
+        return {}
+      end,
+    },
     keymap = {
       preset = "none", -- default
       ["<Tab>"] = {
@@ -19,14 +46,15 @@ return {
         end,
         "fallback",
       },
-      ["<CR>"] = { function(cmp)
+      ["<CR>"] = {
+        function(cmp)
           -- Only accept if the completion menu is currently open
           if cmp.is_menu_visible() then
             return cmp.select_and_accept()
           end
         end,
-        "fallback", },
+        "fallback",
+      },
     },
   },
 }
-
